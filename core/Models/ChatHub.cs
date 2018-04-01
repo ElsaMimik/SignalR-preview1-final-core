59d1d656e3df879d3666529611345e64c8995dc7
柔柔 <mimikapple@gmail.com>
2018-04-01 20:53:15 +0800

    Update ChatHub.cs

--- core/Models/ChatHub.cs
+++ core/Models/ChatHub.cs
@@ -10,36 +10,54 @@ namespace core.Models
     {
         public override async Task OnConnectedAsync()
         {
-            await Clients.All.SendAsync("Send", $"{Context.ConnectionId} joined");
+            //await Clients.All.SendAsync("SendMsg", $"{Context.ConnectionId} joined");
+            await Clients.Caller.SendAsync("SendMsgConsole", $"you are joined");
+            await Clients.Others.SendAsync("GetMsg", $"{Context.ConnectionId} joined");
         }
         public override async Task OnDisconnectedAsync(Exception ex)
         {
-            await Clients.All.SendAsync("Send", $"{Context.ConnectionId} left");
+            //await Clients.All.SendAsync("SendMsg", $"{Context.ConnectionId} left");
+            await Clients.Caller.SendAsync("SendMsgConsole", $"you are left");
+            await Clients.Others.SendAsync("GetMsg", $"{Context.ConnectionId} left");
         }
 
-        public Task Send(string message) => Clients.All.SendAsync("Send", $"{Context.ConnectionId}: {message}");
+        public async Task Send(string message)
+        {
+            await Clients.Caller.SendAsync("SendMsgConsole", $"you are send: {message}");
+            await Clients.Others.SendAsync("GetMsg", $"{Context.ConnectionId}: {message}");
+        }
 
-        public Task SendToSomeone(string receiverId, string message) => Clients.Client(receiverId).SendAsync("Send", $"{Context.ConnectionId}@{receiverId}: {message}");
+        public async Task SendToSomeone(string receiverId, string message)
+        {
+            await Clients.Caller.SendAsync("SendMsgConsole", $"you @ {receiverId}: {message}");
+            await Clients.Client(receiverId).SendAsync("GetMsg", $"{Context.ConnectionId}@{receiverId}: {message}");
+        }
 
-        public Task SendToGroup(string groupName, string message) => Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId}@{groupName}: {message}");
+        public async Task SendToGroup(string groupName, string message)
+        {
+            //await Clients.Group(groupName).SendAsync("GetMsg", $"{Context.ConnectionId}@{groupName}: {message}");
+            await Clients.Caller.SendAsync("SendMsgConsole", $"you @{groupName}: {message}");
+            await Clients.OthersInGroup(groupName).SendAsync("GetMsg", $"{Context.ConnectionId}@{groupName}: {message}");
+        }
 
         public async Task JoinGroup(string groupName)
         {
             await Groups.AddAsync(Context.ConnectionId, groupName);
-
-            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} joined {groupName}");
+            await Clients.Caller.SendAsync("SendMsgConsole", $"you joined {groupName}");
+            await Clients.OthersInGroup(groupName).SendAsync("GetMsg", $"{Context.ConnectionId} joined {groupName}");
         }
 
         public async Task LeaveGroup(string groupName)
         {
             await Groups.RemoveAsync(Context.ConnectionId, groupName);
-
-            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} left {groupName}");
+            await Clients.Caller.SendAsync("SendMsgConsole", $"you left {groupName}");
+            await Clients.OthersInGroup(groupName).SendAsync("GetMsg", $"{Context.ConnectionId} left {groupName}");
         }
 
-        public Task Echo(string message)
+        public async Task Echo(string message)
         {
-            return Clients.Client(Context.ConnectionId).SendAsync("Send", $"{Context.ConnectionId}: {message}");
+            //return Clients.Client(Context.ConnectionId).SendAsync("GetMsg", $"{Context.ConnectionId}: {message}");
+            await Clients.Caller.SendAsync("GetMsg", $"{message}");
         }
     }
 }
