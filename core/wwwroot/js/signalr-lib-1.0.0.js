85dd9e6f4120a4c1f590b4db133dc8c970549a64
柔柔 <mimikapple@gmail.com>
2018-04-13 23:07:24 +0800

    Update signalr-lib-1.0.0.js

--- core/wwwroot/js/signalr-lib-1.0.0.js
+++ core/wwwroot/js/signalr-lib-1.0.0.js
@@ -1,93 +1,88 @@
-﻿/*
+/*
  * JavaScript and TypeScript clients for SignalR for ASP.NET Core
  * library for signalr Dependents signalr.js -version preview1-1.0.0
  * 2018-04-16
  * @
-*/
-var transport = signalR.TransportType.WebSockets
-var connections = []
-var connection = class {
-  constructor(status, hub) {
-    this.status = status
-    this.hub = hub
-  }
-}
-
-function getConnectionHub(connectionHub) {
-  return connections.find(s => s.hub == connectionHub)
-}
-
-function addConnectionHub(connectionHub) {
-  var conn = getConnectionHub(connectionHub)
-  if (conn) {
-    if (!conn.status) {
-      connections.splice(connections.findIndex(s => s.hub === connectionHub), 1)
-      connections.push(new connection(true, connectionHub))
-    }
-  } else {
-    connections.push(new connection(true, connectionHub))
-  }
-}
-
-//regist
-function regist(hub, method, callback) {
-  var connHub = new signalR.HubConnection(hub, { transport: transport })
-  connHub.on(method, msg => {
-    callback()
-  })
-}
-
-//connecting the server to the signalr hub
-function start(hub) {
-  //
-  connection
-    .start()
-    .then(() => {
-      console.log('start')
-      isConnected = true
-      addLine('message-list', 'Connected successfully', 'green')
-    })
-    .catch(err => {
-      addLine('message-list', err, 'red')
-    })
-}
-
-//connecting the server to the signalr hub
-function stop(hub) {
-  //
-  connection
-    .stop()
-    .then(() => {
-      console.log('stop')
-      isConnected = false
-    })
-    .catch(err => {
-      addLine('message-list', err, 'red')
-    })
-}
-
-function retry(hub) {
-  //reconnect
-}
-
-//connecting the server to the signalr hub
-function invoke(connection, method, ...args) {
-  if (!isConnected) {
-    return
-  }
-  var argsArray = Array.prototype.slice.call(arguments)
-  connection.invoke
-    .apply(connection, argsArray.slice(1))
-    .then(result => {
-      console.log(
-        'invocation completed successfully: ' +
-          (result === null ? '(null)' : result)
-      )
-      if (result) {
-        addLine('message-list', result)
-      }
-    })
-    .catch(err => {
-      addLine('message-list', err, 'red')
-    })
+ */
+var HiggsSignalR = {
+    transport: signalR.TransportType.WebSockets,
+    connections: [],
+    connection: class {
+        constructor(status, hub) {
+            this.status = status
+            this.hub = hub
+        }
+    },
+    getConnectionHub: function(connectionHub) {
+        return this.connections.find(s => s.hub == connectionHub)
+    },
+    addConnectionHub: function(connectionHub) {
+        var conn = this.getConnectionHub(connectionHub)
+        if (this.conn) {
+            if (!this.conn.status) {
+                this.connections.splice(this.connections.findIndex(s => s.hub === connectionHub), 1)
+                this.connections.push(new this.connection(true, connectionHub))
+            }
+        } else {
+            this.connections.push(new this.connection(true, connectionHub))
+        }
+    },
+    //regist
+    regist: function(hub, method, callback) {
+        var connHub = new signalR.HubConnection(hub, {
+            transport: transport
+        })
+        connHub.on(method, msg => {
+            callback()
+        })
+    },
+    //connecting the server to the signalr hub
+    start: function() {
+        connection
+            .start()
+            .then(() => {
+                console.log('start')
+                isConnected = true
+                addLine('message-list', 'Connected successfully', 'green')
+            })
+            .catch(err => {
+                addLine('message-list', err, 'red')
+            })
+    },
+    //connecting the server to the signalr hub
+    stop: function() {
+        connection
+            .stop()
+            .then(() => {
+                console.log('stop')
+                isConnected = false
+            })
+            .catch(err => {
+                addLine('message-list', err, 'red')
+            })
+    },
+    retry: function() {
+        //reconnect
+    },
+    //connecting the server to the signalr hub
+    invoke: function() {
+        if (!isConnected) {
+            return
+        }
+        var argsArray = Array.prototype.slice.call(arguments)
+        connection.invoke
+            .apply(connection, argsArray.slice(1))
+            .then(result => {
+                console.log(
+                    'invocation completed successfully: ' +
+                    (result === null ? '(null)' : result)
+                )
+                if (result) {
+                    addLine('message-list', result)
+                }
+            })
+            .catch(err => {
+                addLine('message-list', err, 'red')
+            })
+    },
 }
